275b3ffb4d83bc4f27b4b385310a7ef0abb44bef
jshan <jshan@hanamicron.co.kr>
2022-03-02 09:33:20 +0900

    [#11]rendering driver in real time

--- uberPractice/Service/Service.swift
+++ uberPractice/Service/Service.swift
@@ -8,6 +8,7 @@
 import Firebase
 import CoreLocation
 import GeoFire
+import UIKit
 let DB_REF = Database.database().reference()
 let REF_USERS = DB_REF.child("users")
 let REF_DRIVER_LOCATIONS = DB_REF.child("driver-locations")
@@ -18,23 +19,28 @@ struct Service {
     private init(){}
     
     
-    func fetchUserdata(completion : @escaping (User)->Void) {
-        guard let currentUid = Auth.auth().currentUser?.uid else {return}
-        REF_USERS.child(currentUid).observeSingleEvent(of: .value) { snapshot in
+    func fetchUserdata(uid:String,completion : @escaping (User)->Void) {
+        
+        REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshot in
             guard let data = snapshot.value as? [String:Any] else {return}
-            let user = User.init(dictionary: data)
+            let user = User.init(uid: uid, dictionary: data)
             
             completion(user)
         }
     }
-    
-    func fetchDrivers(location : CLLocation) {
+    // 데이터 변경될때마다 호출
+    func fetchDrivers(location : CLLocation,completion : @escaping (User)->Void) {
         let geofire = GeoFire(firebaseRef: REF_DRIVER_LOCATIONS)
         REF_DRIVER_LOCATIONS.observe(.value) { snapshot in
             geofire.query(at: location, withRadius: 50).observe(.keyEntered,with: { uid, location in
-                
+                self.fetchUserdata(uid: uid) { user in
+                    var driver = user
+                    driver.location = location
+                    completion(driver)
+                }
             })
             
         }
     }
+    
 }
